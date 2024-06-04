8037669b99d8cae2f5de84dd48aa4d3c8c349ffe
Syahmi Muqri <m.syahmi@alami.my>
2024-06-04 12:10:14 +0800

    End of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Fragment, useEffect } from 'react'
 import { Container } from 'semantic-ui-react'
 import Navbar from './NavBar'
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
-import LoadingComponent from './LoadingComponent'
-import { useStore } from '../stores/store'
 import { observer } from 'mobx-react-lite'
+import { Outlet, useLocation } from 'react-router-dom'
+import HomePage from '../../features/home/HomePage'
 
 function App() {
-  const {activityStore} = useStore()
-
-  useEffect(() => {
-    activityStore.loadActivities()        
-  }, [activityStore])
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  const location = useLocation()
 
   return (
-    <Fragment>
-      <Navbar />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
-      </Container>
-    </Fragment>
+    <>
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <Navbar />
+          <Container style={{marginTop: '7em'}}>
+            <Outlet />
+          </Container>
+        </>
+      )}
+    </>
   )
 }
 
